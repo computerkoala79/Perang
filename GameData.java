a7242d736c6dd74df10b1c5aedcc527a1d0242ad
Jerry Klos <jerry.klos@ucdconnect.ie>
2021-04-17 14:28:29 +0100

    updated GameData

--- GameData.java
+++ GameData.java
@@ -3,16 +3,43 @@
  */
 public class GameData {
 	
-	public static final int FULL_DECK_SIZE = 3;
+	public static final int FULL_DECK_SIZE = 16;
 	public static final String[][] CARD_NAMES = {
 			{"Jerry","Creator"},
 			{"Liakada","Unknown"},
-			{"Sebastian","Meenun"}};
+			{"Sebastian","Meenun"},
+			{"Draven","Unknown"},
+			{"Terislee","Unknown"},
+			{"The Unknowing","Unknown"},
+			{"Temil","Tembian"},
+			{"Echo","Talonian"},
+			{"Leila","Talonian"},
+			{"Jarrett","Descendant"},
+			{"FantaC","Siren"},
+			{"Pheonix","Eternal"},
+			{"Everscott","Unknown"},
+			{"Lalen","Ancient"},
+			{"Our Mother","Eternal"},
+			{"Our Fathers","Eternal"},
+			};
 	public static final int[][] ATTACK_STATS = 
 		{
 			{9,9,9}, // Jerry
 			{6,5,4}, // Liakada
-			{3,4,5}  // Sebastian
+			{3,4,5},  // Sebastian
+			{9,9,9}, // Jerry
+			{9,9,9}, // Jerry
+			{9,9,9}, // Jerry
+			{9,9,9}, // Jerry
+			{9,9,9}, // Jerry
+			{9,9,9}, // Jerry
+			{9,9,9}, // Jerry
+			{9,9,9}, // Jerry
+			{9,9,9}, // Jerry
+			{9,9,9}, // Jerry
+			{9,9,9}, // Jerry
+			{9,9,9}, // Jerry
+			{9,9,9}, // Jerry
 			};
 	public static final int[][] DEFENSE_STATS = 
 		{
